e6dab73c31bc6500da548bb1ee3b9ee0fcc8e688
Rian Mendonça <rianmendonca@proton.me>
2023-11-30 13:43:25 -0300

    Conexão com banco de dados criada

---
+++ login-system/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace login_system.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
