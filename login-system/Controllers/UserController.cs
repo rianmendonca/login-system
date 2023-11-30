972726d559100eb356024547aa59f047c42227b0
Rian Mendonça <rianmendonca@proton.me>
2023-11-30 14:24:52 -0300

    Função de cadastro adicionada

--- login-system/Controllers/UserController.cs
+++ login-system/Controllers/UserController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using login_system.Models;
+using login_system.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace login_system.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUser(UserModel user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    user = _userRepository.RegisterUser(user);
+
+                    TempData["SuccessMessage"] = "Cadastro realizado com sucesso!";
+                    return View("Index");
+                }
+                return View(user);
+            }
+            catch (Exception erro)
+            {
+                TempData["ErrorMessage"] = $"Não foi possivel realizar o cadastro, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
--- login-system/Program.cs
+++ login-system/Program.cs
@@ -1,4 +1,5 @@
 using login_system.Data;
+using login_system.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
